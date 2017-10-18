dca4102327c9ab2b4ba75f25babb825a16589798
nramiscal <nramiscal@gmail.com>
2017-10-18 18:43:03 -0400

    refactored urls

---
+++ main/apps/blogs/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^blogs$', views.index),
+    url(r'^blogs/$', views.index),
+    url(r'^blogs/new$', views.new),
+    url(r'^blogs/create$', views.create),
+    url(r'^blogs/(?P<number>\d+)/$', views.show),
+    url(r'^blogs/(?P<number>\d+)/edit$', views.edit),
+    url(r'^blogs/(?P<number>\d+)/delete$', views.destroy),
+]
